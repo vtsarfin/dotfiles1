cc032cb41748a2415fe16046852272e7431b5560
Vadim Tsarfin <vtsarfin@gmail.com>
2022-04-11 20:16:24 +0300

    added pyright

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -3,6 +3,7 @@ local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
   packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
+require'lspconfig'.pyright.setup{}
 return require('packer').startup(function()
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
