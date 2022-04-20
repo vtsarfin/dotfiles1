d7d69c874bdfd4608470f2da8c18e9d7e64d3ce1
Vadim Tsarfin <vtsarfin@gmail.com>
2022-04-20 14:39:56 +0300

    added hello

---
+++ hellovt.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+print("hello guys!")
+print();
+ebani_rot={};
+ebani_rot={
+        "gavno":5,
+        "mocha":6,
+        "sluni":9,
+        
+        }
+print(ebani_rot['gavno']);
+print(list(ebani_rot.keys()));
+for i in range(2):
+    for i in ebani_rot.items():
+        print(i)
+    else:
+        print("huizalupa")
+print(list(ebani_rot.keys())[:-1]);
+print(len([ebani_rot['gavno']]));
+n_list=[n for n in ebani_rot.values() if n % 2 == 1]
+print(n_list);
+
+
