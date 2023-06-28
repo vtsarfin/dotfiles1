0a139bdb0eb52b06f3deeecd6ba3a5387e555fe1
VADIM TSARFIN <vtsarfin@gmail.com>
2023-06-28 21:36:31 +0300

    added terraform-ls

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -91,12 +91,13 @@ return require('packer').startup(function()
   })
 
   -- Setup lspconfig.
-  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
   -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
   require('lspconfig')['pyright'].setup {
     capabilities = capabilities
   }
-  require'lspconfig'.sumneko_lua.setup {
+  require'lspconfig'.perlpls.setup{}
+  require'lspconfig'.lua_ls.setup {
     settings = {
       Lua = {
         runtime = {
@@ -118,7 +119,11 @@ return require('packer').startup(function()
       },
     },
   }
-
+require'lspconfig'.terraformls.setup{}
+-- vim.api.nvim_create_autocmd({"BufWritePre"}, {
+--  pattern = {"*.tf", "*.tfvars"},
+--  callback = vim.lsp.buf.format(),
+-- })
 end
 )
 
